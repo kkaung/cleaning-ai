8e465528369a77142d2191702bda446bf8c7bb47
moonGoblin <kaungzaw1996@gmail.com>
2024-04-20 17:38:09 +1000

    fixed: home-page

--- src/app/(marketing)/_components/cloaked-content.tsx
+++ src/app/(marketing)/_components/cloaked-content.tsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import { Mdx } from '@/components/mdx/mdx-components';
 import { Shell } from '@/components/shell';
-import { allPages, allServices } from 'contentlayer/generated';
+import { allServices } from 'contentlayer/generated';
 
 export default function CloakedContent() {
-    // const service = allServices.find(v => v.slugAsParams === 'house-cleaning')!;
-
-    // console.log(service.slugAsParams);
-
-    const page = allPages.find(p => p.slugAsParams === 'contact');
-
-    if (!page) return <div>No</div>;
+    const page = allServices.find(v => v.slugAsParams === 'house-cleaning')!;
 
     return (
         <Shell as="article" variant="markdown">
